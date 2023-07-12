fdb7f972268cc78e10e527e5e7b01e3afb155fb5
Cleve <clevemomanyi@gmail.com>
2023-07-12 19:29:25 +0300

    Finally getting closer

---
+++ Challenges/newsletter-signup/index.js
@@ -0,0 +1,51 @@
+// Variables from UI
+
+// const inputEmail = document.getElementsByClassName('email')[0]
+// const subscribeBtn = document.getElementsByClassName('subscribe')[0]
+
+/*
+    Tried using "document.querySelector()" first but it didn't work out,
+    I later realised that It was because I hadn't set the "defer" attribute 
+    when linking the Js file :)
+
+*/
+
+// Global UI components
+const inputEmail = document.querySelector('.email')
+const primaryContainer = document.querySelector('.container')
+const secondaryContainer = document.querySelector('.success-container')
+
+
+// Buttons
+const subscribeBtn = document.querySelector('.subscribe')
+const dismissBtn = document.querySelector('.dismiss')
+
+// Variable to check if email is correct
+const submit = true;
+
+// Function to hide UI
+const hide = function(el){
+  return  el.classList.add('hidden');
+}
+
+// Functionto display UI
+const display = function(el){
+    return el.classList.remove('hidden')
+}
+
+// Listening for the submit event.
+subscribeBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    if(inputEmail.value === "clevemomanyi@gmail.com") {
+        hide(primaryContainer)
+        display(secondaryContainer)
+        inputEmail.value = ''
+    }
+})
+
+// Returning to primary container on dismiss btn click
+dismissBtn.addEventListener('click', function(){
+    hide(secondaryContainer);
+    display(primaryContainer)
+})
+
