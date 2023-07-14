47e1cd4e760bbf388d4cd9be4b7a8f4046d0e35d
Cleve <clevemomanyi@gmail.com>
2023-07-14 19:27:41 +0300

    Beta version

--- Challenges/advice-generator-app-main/index.js
+++ Challenges/advice-generator-app-main/index.js
@@ -1,15 +1,23 @@
 const quoteText =  document.querySelector('.text');
 const quoteNumber = document.querySelector('.quoteNumber')
+const diceBtn = document.querySelector('.dice')
 
-const quote = fetch(`https://quote-garden.onrender.com/api/v3/quotes/random`)
-.then((res) => res.json())
-.then((data) => {
+
+const generateQuote = function(){
+   return fetch(`https://quote-garden.onrender.com/api/v3/quotes/random`)
+    .then((res) => res.json())
+    .then((data) => {
     const randomNumber = Math.trunc(Math.random() * 200) + 1;
     const randomQuote = `"${data.data[0].quoteText}"`;
     quoteText.textContent = randomQuote;
     quoteNumber.textContent = randomNumber;
+    })
+    .catch(err => console.log(err))
+}
+
+diceBtn.addEventListener('click', function(){
+    generateQuote();
 })
-.catch(err => console.log(err))
 
 
 
