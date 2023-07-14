ce5a189d86e3b9cb3c7be1f4b941dce78fdf4459
Cleve <clevemomanyi@gmail.com>
2023-07-14 10:36:13 +0300

    Almost

--- Challenges/Multistep-form/index.js
+++ Challenges/Multistep-form/index.js
@@ -1,19 +1,37 @@
+/* Check for previous code implementation and thought process at the end of the code */  
+
 // Global variables
 const btnNext = document.querySelectorAll('.next');
 const btnBack = document.querySelectorAll('.back');
-console.log(btnNext, btnBack)
+const btnSubmit = document.querySelector('.submit')
 
-// Containers
-const step1 = document.querySelector('.step1Container')
-const step2 = document.querySelector('.step-2-container')
-console.log(step1, step2)
+// Steps
+const steps = document.querySelectorAll('.content')
+const stepBtn = document.querySelectorAll('.stepBtn')
 
+
+const showStep = function(stepIndex){
+    if (stepIndex < 0 || stepIndex >= steps.length) {
+        return; // Check for invalid step index
+      }
+      steps[stepIndex].classList.remove('hidden');
+      stepBtn[stepIndex].classList.toggle('active');
+}
+
+const hideStep = function(stepIndex){
+    if (stepIndex < 0 || stepIndex >= steps.length) {
+        return;
+    }
+    steps[stepIndex].classList.add('hidden');
+    stepBtn[stepIndex].classList.toggle('active');
+}
 
 // Current step index
 let currentStep = 0;
 
 // Go to next page feature
-btnNext.addEventListener('click', function(e) {
+btnNext.forEach((button)=>{
+button.addEventListener('click', function(e) {
     e.preventDefault();
     // Hide current step
     hideStep(currentStep);
@@ -24,34 +42,81 @@ btnNext.addEventListener('click', function(e) {
     // Show next step
     showStep(currentStep);
 });
+})
+
 
 // Go to previous page feature
-btnBack.addEventListener('click', function(e) {
-    e.preventDefault();
+btnBack.forEach((button) =>{
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+        // Hide current step
+        hideStep(currentStep);
+    
+        // Decrement current step index
+        currentStep--;
+    
+        // Show previous step
+        showStep(currentStep);
+    });
+})
+
+// Submit button
+btnSubmit.addEventListener('click', function(){
     // Hide current step
-    hideStep(currentStep);
+    hideStep(currentStep)
 
-    // Decrement current step index
-    currentStep--;
+    // Increment step counter
+    currentStep++;
 
-    // Show previous step
-    showStep(currentStep);
-});
+    // Show next step
+    showStep(currentStep)
+})
+
+
+
+
+
+
+//////////////////////////////////////////////////////////
+// Version 1
+
+// Containers
+// const step1 = document.querySelector('.step1Container');
+// const step2 = document.querySelector('.step-2-container');
+// const step3 = document.querySelector('.step-3-container');
+// const step4 = document.querySelector('.step-4-container');
+// const lastContainer = document.querySelector('.successContainer')
 
 // Function to hide a step
-function hideStep(stepIndex) {
-    if (stepIndex === 0) {
-        step1.classList.add('hidden')
-    } else if (stepIndex === 1) {
-        step2.classList.add('hidden')
-    }
-}
+// function hideStep(stepIndex) {
+//     if (stepIndex === 0) {
+//         step1.classList.add('hidden')
+//     } else if (stepIndex === 1) {
+//         step2.classList.add('hidden')
+//     } else if (stepIndex === 2){
+//         step3.classList.add('hidden')
+//     } else if (stepIndex === 3){
+//         step3.classList.add('hidden')
+//     } else if (stepIndex === 4){
+//         step4.classList.add('hidden');
+//     } else if (stepIndex == 4){
+//         lastContainer.classList.add('hidden')
+//     }
+// }
 
 // Function to show a step
-function showStep(stepIndex){
-    if (stepIndex === 0){
-        step1.classList.remove('hidden')
-    } else if (stepIndex === 1){
-        step2.classList.remove('hidden')
-    }
-}
+// function showStep(stepIndex){
+//     if (stepIndex === 0){
+//         step1.classList.remove('hidden')
+//     } else if (stepIndex === 1){
+//         step2.classList.remove('hidden')
+//     } else if (stepIndex === 2){
+//         step3.classList.remove('hidden')
+//     } else if (stepIndex === 3){
+//         step3.classList.remove('hidden')
+//     } else if (stepIndex === 4){
+//         step4.classList.remove('hidden');
+//     } else if (stepIndex == 4){
+//         lastContainer.classList.add('hidden')
+//     }
+// }
