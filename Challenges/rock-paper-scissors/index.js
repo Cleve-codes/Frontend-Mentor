7d0efc91e5351a0b66945fd66d5be512a8b58891
Cleve <clevemomanyi@gmail.com>
2023-07-15 19:50:35 +0300

    We moving to 3 now

--- Challenges/rock-paper-scissors/index.js
+++ Challenges/rock-paper-scissors/index.js
@@ -1,6 +1,7 @@
 // Global Variables
 const selection = document.querySelector('.selection');
 const rules = document.querySelector('.rules');
+const primary = document.querySelector('.primary')
 
 const rulesBtn = document.querySelector('.rulesBtn');
 const closeBtn = document.querySelector('.close')
@@ -11,22 +12,33 @@ let modalOpen = false;
 const openModal = function(){
     selection.classList.add('hidden');
     rules.classList.remove('hidden')
-
+    modalOpen = true;
 }
 
 // Function to hide rules
 const closeModal = function(){
     rules.classList.add('hidden');
     selection.classList.remove('hidden')
+    modalOpen = false;
 }
 
 // EventListeners
 // Show Rules on rulesBtn click
+
 rulesBtn.addEventListener('click', function(){
     if(!modalOpen) openModal();
-    selection.classList.add('overlay')
+    body.classList.add('overlay')
 });
 
 // Close modal on clicking close btn
-closeBtn.addEventListener('click', closeModal)
+closeBtn.addEventListener('click', function(){
+    closeModal();
+    primary.classList.remove('overlay');
+})
 
+// Close on pressing escape{
+if(!modalOpen){
+    window.addEventListener('keydown', function(e){
+        if(e.key = 'Escape') closeModal();
+    })
+}
