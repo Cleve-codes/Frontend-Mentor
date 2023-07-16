262557f92bc93df8ef0e655ff00cdb043cb4cf7f
Cleve <clevemomanyi@gmail.com>
2023-07-16 18:11:29 +0300

    1step to final

--- Challenges/rock-paper-scissors/index.js
+++ Challenges/rock-paper-scissors/index.js
@@ -1,18 +1,15 @@
 // Global Variables
 const pages = Array.from(document.querySelectorAll('.page'));
+console.log(pages)
+console.log(pages[0], pages[1], pages[2])
 const primary = document.querySelector('.primary');
 
 const selection = document.querySelector('.selection');
-
 const select = Array.from(document.querySelectorAll('.img-choice'));
-
-// const paper = document.querySelector('.paper');
-// const rock = document.querySelector('.rock');
-// const scissors = document.querySelector('.scissors');
 
 const rulesBtn = document.querySelector('.rulesBtn');
 const closeBtn = document.querySelector('.close');
-const playAgainBtn = document.querySelector('.playAgainBtn');
+const playAgainBtn = document.querySelector('.playAgain-btn');
 
 let activePage = 0;
 
@@ -26,9 +23,8 @@ let activePage = 0;
 selection.addEventListener('click', function(e){ 
     // If clicked element is not same as random Pick
     const randomNumber = Math.floor(Math.random()* 3);
-    console.log(randomNumber, "Random number")
     const randomPick = select[randomNumber];
-    console.log(randomPick)
+   
 
      if(randomPick !== e.target){
         console.log(e.target, "Event target")
@@ -36,25 +32,29 @@ selection.addEventListener('click', function(e){
             // hidePage(pages[0]);
             pages[0].style.display = 'none'
             showPage(pages[1])
-            console.log('Paper')
+            hideBtn.style.display = 'none'
+           
         }
       
         if(e.target === select[1] && randomPick === select[2] || e.target === select[2] && randomPick === select[1]){
             // hidePage(pages[0]);
             pages[0].style.display = 'none'
             showPage(pages[1])
-            console.log('rock')
+            hideBtn.style.display = 'none'
+          
         }
         else if(e.target === select[2] && randomPick === select[0] || e.target === select[0] && randomPick === select[2]){
             // hidePage(pages[0])
             pages[0].style.display = 'none'
             showPage(pages[1])
-            console.log('Scissors')
+            hideBtn.style.display = 'none'
+          
         } 
     } else{
         pages[0].style.display = 'none'
         showPage(pages[1])
-        console.log("Draw")
+        hideBtn.style.display = 'none'
+        
     }
 
 })
@@ -62,14 +62,21 @@ selection.addEventListener('click', function(e){
 //////////////////////////////////////////////////////////////////////
 // Function to show page
 const showPage = function(page){
-    page.classList.remove('hidden')
+    page.classList.remove('hidden');
 }
 
 // Function to hide page
 const hidePage = function(page){
-    page.classList.add('hidden')
+    page.classList.add('hidden');
 }
 
+const hideBtn = function(){
+    rulesBtn.classList.add('hidden');
+}
+
+const showBtn = function(){
+    rulesBtn.classList.remove('hidden');
+}
 
 // EventListeners
 ///////////////////////////////////////////////////////////////////////
@@ -77,14 +84,14 @@ const hidePage = function(page){
 rulesBtn.addEventListener('click', function(){
     showPage(pages[2]);
     primary.classList.add('overlay')
-    rulesBtn.classList.add('hidden')
+    hideBtn()
 });
 
 // Close modal on clicking close btn
 closeBtn.addEventListener('click', function(){
     hidePage(pages[2]);
     showPage(pages[0])
-    rulesBtn.classList.remove('hidden')
+    showBtn();
     primary.classList.remove('overlay')
 })
 
@@ -95,7 +102,7 @@ primary.addEventListener('click', function(e){
         hidePage(pages[2]);
         showPage(pages[0]);
         primary.classList.remove('overlay')
-        rulesBtn.classList.remove('hidden')
+        showBtn();
     }
 })
 
@@ -105,10 +112,18 @@ document.addEventListener('keydown', function(e){
         hidePage(pages[2]);
         showPage(pages[0]);
         primary.classList.remove('overlay')
-        rulesBtn.classList.remove('hidden')
+        showBtn();
     }
 })
 
+//////////////////////////////////////////////////////////////////
+// Play Again button
 
-
+playAgainBtn.addEventListener('click', function(){
+    pages[0].style.display = 'block'
+    hidePage(pages[1]);
+    hidePage(pages[2])
+    showBtn()
+    console.log(pages)
+})
 
