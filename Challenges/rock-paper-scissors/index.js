ca8b63ec59dd20e512909a25e2fda4212e4917c2
Cleve <clevemomanyi@gmail.com>
2023-07-17 22:17:52 +0300

    Remaining Scores

--- Challenges/rock-paper-scissors/index.js
+++ Challenges/rock-paper-scissors/index.js
@@ -1,13 +1,19 @@
 // Global Variables
-const pages = Array.from(document.querySelectorAll('.page'));
-const primary = document.querySelector('.primary');
+const pages = Array.from(document.querySelectorAll(".page"));
+const primary = document.querySelector(".primary");
+const message = document.querySelector(".message");
 
-const selection = document.querySelector('.selection');
-const select = Array.from(document.querySelectorAll('.img-choice'));
+const selection = document.querySelector(".selection");
+const select = Array.from(document.querySelectorAll(".img-choice"));
 
-const rulesBtn = document.querySelector('.rulesBtn');
-const closeBtn = document.querySelector('.close');
-const playAgainBtn = document.querySelector('.playAgain-btn');
+const rulesBtn = document.querySelector(".rulesBtn");
+const closeBtn = document.querySelector(".close");
+const playAgainBtn = document.querySelector(".playAgain-btn");
+
+const user = document.querySelector(".user");
+const house = document.querySelector(".house");
+
+const cards = ["card1", "card2", "card3"];
 
 let activePage = 0;
 
@@ -16,157 +22,113 @@ let activePage = 0;
 // Rock BEATS Scissors
 // Paper BEATS Rock
 
-let winner = ''; 
-let loser = '';
+let randomNumber, randomPick, userChoice;
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Version 1
-selection.addEventListener('click', function(e){ 
-    // If clicked element is not same as random Pick
-    const randomNumber = Math.floor(Math.random()* 3);
-    const randomPick = select[randomNumber];
+selection.addEventListener("click", function (e) {
+  // If clicked element is not same as random Pick
+  const randomNumber = Math.floor(Math.random() * 3);
+  const randomPick = select[randomNumber];
 
-    const user = document.querySelector('.user');
-    const house = document.querySelector('.house')
-   
+  const userChoice = select.indexOf(e.target);
+  handleWinner(userChoice, randomNumber);
 
-     if(randomPick !== e.target){
-        console.log(e.target, "Event target")
-        if(e.target === select[0] && randomPick === select[1] || e.target === select[1] && randomPick === select[0]){
-            // hidePage(pages[0]);
-            pages[0].style.display = 'none'
-            showPage(pages[1])
-            
-          let htmlMarkupW = `<div class="select card1">
-                                <img src="images/icon-paper.svg" alt="" class="img-choice">
-                            </div>`
-
-            let htmlMarkupL = `<div class="select card1">
-                                    <img src="images/icon-paper.svg" alt="" class="img-choice">
-                                </div>`
-
-            user.innerHTML = htmlMarkupW;
-            house.innerHTML = htmlMarkupL;
-        }
-      
-        if(e.target === select[1] && randomPick === select[2] || e.target === select[2] && randomPick === select[1]){
-            // hidePage(pages[0]);
-            pages[0].style.display = 'none'
-            showPage(pages[1])
-           
-            let htmlMarkupW = `<div class="select card1">
-                            <img src="images/icon-paper.svg" alt="" class="img-choice">
-                        </div>`
-
-            let htmlMarkupL = `<div class="select card1">
-                                <img src="images/icon-paper.svg" alt="" class="img-choice">
-                            </div>`
-
-            user.innerHTML = htmlMarkupW;
-            house.innerHTML = htmlMarkupL; 
-           
-        }
-        else if(e.target === select[2] && randomPick === select[0] || e.target === select[0] && randomPick === select[2]){
-            // hidePage(pages[0])
-            pages[0].style.display = 'none'
-            showPage(pages[1])
-           
-            let htmlMarkupW = `<div class="select card1">
-                                <img src="images/icon-paper.svg" alt="" class="img-choice">
-                            </div>`
-
-            let htmlMarkupL = `<div class="select card1">
-                                    <img src="images/icon-paper.svg" alt="" class="img-choice">
-                                </div>`
-
-            user.innerHTML = htmlMarkupW;
-            house.innerHTML = htmlMarkupL;
-         
-        } 
-    } else{
-        pages[0].style.display = 'none'
-        showPage(pages[1])
-        
-    
-        let htmlMarkupW = `<div class="select card1">
-                                <img src="images/icon-paper.svg" alt="" class="img-choice">
-                            </div>`
-
-            let htmlMarkupL = `<div class="select card1">
-                                    <img src="images/icon-paper.svg" alt="" class="img-choice">
-                                </div>`
-
-        user.innerHTML = htmlMarkupW;
-        house.innerHTML = htmlMarkupL;
-    }
-
-})
+  pages[0].style.display = "none";
+  showPage(pages[1]);
+});
 
 //////////////////////////////////////////////////////////////////////
 // Function to show page
-const showPage = function(page){
-    page.classList.remove('hidden');
-}
+const showPage = function (page) {
+  page.classList.remove("hidden");
+};
 
 // Function to hide page
-const hidePage = function(page){
-    page.classList.add('hidden');
-}
+const hidePage = function (page) {
+  page.classList.add("hidden");
+};
 
-const hideBtn = function(){
-    rulesBtn.classList.add('hidden');
-}
+const hideBtn = function () {
+  rulesBtn.classList.add("hidden");
+};
 
-const showBtn = function(){
-    rulesBtn.classList.remove('hidden');
-}
+const showBtn = function () {
+  rulesBtn.classList.remove("hidden");
+};
 
 // EventListeners
 ///////////////////////////////////////////////////////////////////////
 // Open modal on rulesBtn click
-rulesBtn.addEventListener('click', function(){
-    showPage(pages[2]);
-    primary.classList.add('overlay')
-    hideBtn()
+rulesBtn.addEventListener("click", function () {
+  showPage(pages[2]);
+  primary.classList.add("overlay");
+  hideBtn();
 });
 
 // Close modal on clicking close btn
-closeBtn.addEventListener('click', function(){
-    hidePage(pages[2]);
-    showPage(pages[0])
-    showBtn();
-    primary.classList.remove('overlay')
-})
+closeBtn.addEventListener("click", function () {
+  hidePage(pages[2]);
+  showPage(pages[0]);
+  showBtn();
+  primary.classList.remove("overlay");
+});
 
 // Close modal on clicking anywhere outside the modal
-primary.addEventListener('click', function(e){
-
-    if(e.target !== pages[2]){   
-        hidePage(pages[2]);
-        showPage(pages[0]);
-        primary.classList.remove('overlay')
-        showBtn();
-    }
-})
+primary.addEventListener("click", function (e) {
+  if (e.target !== pages[2]) {
+    hidePage(pages[2]);
+    showPage(pages[0]);
+    primary.classList.remove("overlay");
+    showBtn();
+  }
+});
 
 // Close modal on pressing "Esc" key
-document.addEventListener('keydown', function(e){
-    if(e.key === "Escape"){
-        hidePage(pages[2]);
-        showPage(pages[0]);
-        primary.classList.remove('overlay')
-        showBtn();
-    }
-})
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    hidePage(pages[2]);
+    showPage(pages[0]);
+    primary.classList.remove("overlay");
+    showBtn();
+  }
+});
 
 //////////////////////////////////////////////////////////////////
 // Play Again button
 
-playAgainBtn.addEventListener('click', function(){
-    pages[0].style.display = 'block'
-    hidePage(pages[1]);
-    hidePage(pages[2])
-    hideBtn()
-    console.log(pages)
-})
+playAgainBtn.addEventListener("click", function () {
+  pages[0].style.display = "block";
+  hidePage(pages[1]);
+  hidePage(pages[2]);
+  hideBtn();
+  console.log(pages);
+});
 
+// Function to handle winner
+const handleWinner = (userChoice, randomNumber) => {
+  let htmlMarkupW = `<div class="select ${cards[userChoice]}">
+                          <img src="${select[userChoice].src}" alt="" class="img-choice">
+                        </div>`;
+  let htmlMarkupL = `<div class="select ${cards[randomNumber]}">
+                          <img src="${select[randomNumber].src}" alt="" class="img-choice">
+                        </div>`;
+
+  if (userChoice === randomNumber) {
+    message.innerText = "DRAW!";
+    user.innerHTML = htmlMarkupL;
+    house.innerHTML = htmlMarkupL;
+  } else if (
+    (userChoice === 0 && randomNumber === 1) ||
+    (userChoice === 1 && randomNumber === 2) ||
+    (userChoice === 2 && randomNumber === 0)
+  ) {
+    message.innerText = "You Win";
+    user.innerHTML = htmlMarkupW;
+    house.innerHTML = htmlMarkupL;
+  } else {
+    message.innerText = "You Lose";
+    user.innerHTML = htmlMarkupW;
+    house.innerHTML = htmlMarkupL;
+  }
+};
