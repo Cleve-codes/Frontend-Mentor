434e99ff213527f0702198a3d0b92d6b6435e83e
Cleve <clevemomanyi@gmail.com>
2023-07-17 15:25:27 +0300

    Part 2 Done

--- Challenges/Snake game/script.js
+++ Challenges/Snake game/script.js
@@ -1,24 +1,75 @@
 const playBoard = document.querySelector('.play-board')
 
+let gameOver = false;
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
+let snakeBody = [];
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPosition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const handleGameOver =  () => {
+    alert("Game over my boy :)")
+}
+
 const changeDirection = (e) => {
-    console.log(e)
+    // Change velocity value based on key press
+    if(e.key === 'ArrowUp'){
+        velocityX = 0;
+        velocityY = -1
+    } else if(e.key === 'ArrowDown'){
+        velocityX = 0;
+        velocityY = 1;
+    } else if(e.key === 'ArrowLeft'){
+        velocityX = -1;
+        velocityY = 0;
+    } else if(e.key === 'ArrowRight'){
+        velocityX = 1;
+        velocityY = 0;
+    }
+    
 }
 
 const initGame = () => {
+    if(gameOver) return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area : ${foodY} / ${foodX}"></div>`;
-    htmlMarkup += `<div class="head" style="grid-area : ${snakeY} / ${snakeX}"></div>`
-    playBoard.innerHTML = htmlMarkup;
+
+    // Check if snake ate the food
+    if(snakeX === foodX && snakeY === foodY){
+        // Change food position
+        changeFoodPosition(); 
+        // Add food to snake body;
+        snakeBody.push([foodX, foodY])
+        console.log(snakeBody);
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--){
+        // Shifting forward the value of the html elements in the snake body by one
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY] // Setting first element of snake body to current snake position
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    // Check if snake head is out of the play-ground
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+        // console.log('Game Over')
+        gameOver = true;
+    }
+
+    for(i = 0; i < snakeBody.length; i++){
+        htmlMarkup += `<div class="head" style="grid-area : ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`
+    } 
+    playBoard.innerHTML = htmlMarkup; 
+
 }
 
 changeFoodPosition();
-initGame();
+setInterval(initGame, 125);
 
 document.addEventListener('keydown', changeDirection)
